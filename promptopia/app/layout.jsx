5c8a46a9ce47257e281961a66775ec2d2366eee1
Sadra_Taghizadeh <110200987+Sadra-Taghizadeh@users.noreply.github.com>
2023-07-07 20:23:58 +0330

    new update

--- promptopia/app/layout.jsx
+++ promptopia/app/layout.jsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
-
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 export const metadata = {
     title: "Promptopia",
     description: "Discover & Share AI Prompts",
@@ -14,7 +15,7 @@ const RootLayout = ({ children }) => (
                 </div>
 
                 <main className='app'>
-                    
+                    <Nav />
                     {children}
                 </main>
             
--- promptopia/app/page.jsx
+++ promptopia/app/page.jsx
@@ -1,4 +1,4 @@
-
+import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -11,6 +11,8 @@ const Home = () => {
               Promptopia is an open-source AI prompting tool for modern world to
       discover, create and share creative prompts 
         </p>
+
+        <Feed />
     </section>
     )
 }
